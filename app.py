8321e13cefe13f03d56769472b9e384cc9e7df64
Ramzan Asghar <ramzanasghar0300@gmail.com>
2026-05-17 22:33:28 +0500

    new working groq code

--- app.py
+++ app.py
@@ -1,47 +1,24 @@
 import streamlit as st
-from langchain_groq import ChatGroq
+from groq import Groq
 
-st.set_page_config(
-    page_title="AI Agent",
-    page_icon="🤖",
-    layout="centered"
+st.title("🤖 AI Agent")
+
+client = Groq(
+    api_key=st.secrets["GROQ_API_KEY"]
 )
 
-st.title("🤖 AI Assistant")
-st.write("Ask me anything!")
+question = st.text_input("Ask Anything")
 
-api_key = "YOUR_GROQ_KEY"
+if st.button("Ask AI"):
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-prompt = st.chat_input("Type your question...")
-
-if prompt:
-
-    st.session_state.messages.append(
-        {"role": "user", "content": prompt}
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": question,
+            }
+        ],
+        model="llama-3.1-8b-instant",
     )
 
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    llm = ChatGroq(
-        groq_api_key=api_key,
-        model_name="llama-3.3-70b-versatile"
-    )
-
-    response = llm.invoke(prompt)
-
-    ai_response = response.content
-
-    st.session_state.messages.append(
-        {"role": "assistant", "content": ai_response}
-    )
-
-    with st.chat_message("assistant"):
-        st.markdown(ai_response)
+    st.write(chat_completion.choices[0].message.content)
